ddaedc53b480458e7bac8f0f2b0979ad6427dcd7
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2024-02-13 12:58:48 +0100

    [serverless] stub telemetry

--- pkg/telemetry/stat_counter_wrapper.go
+++ pkg/telemetry/stat_counter_wrapper.go
@@ -3,6 +3,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build !serverless
+// +build !serverless
+
 // Package telemetry provides types and functions for internal telemetry
 package telemetry
 
