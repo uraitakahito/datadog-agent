6ee95b8c2cba15e8f3a6f666c1611637c69241d8
Remy Mathieu <remy.mathieu@datadoghq.com>
2024-03-21 11:43:52 +0100

    udpate sds omnibus software definition

--- omnibus/config/software/sds.rb
+++ omnibus/config/software/sds.rb
@@ -1,12 +1,18 @@
 name "sds"
 
+# default_version "4112a1c"
+default_version "main"
+
+source git: 'https://github.com/DataDog/dd-sensitive-data-scanner'
+
+relative_path "sds-go/rust"
+
 build do
     license "Apache-2.0"
-    source git: 'https://github.com/DataDog/dd-sensitive-data-scanner.git'
-
-    relative_path "dd-sensitive-data-scanner/sds-go/rust"
+    license_file "./LICENSE"
 
     if linux_target?
+        command "ls -lah"
         command "cargo build --release"
         command "cp target/release/libsds_go.so #{install_dir}/embedded/lib"
     end
