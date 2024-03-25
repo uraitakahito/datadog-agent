f4c4632e05640b9f37bf7517df070ac246e42a49
Remy Mathieu <remy.mathieu@datadoghq.com>
2024-03-25 15:13:25 +0100

    use cwd to move in the repo

--- omnibus/config/software/sds.rb
+++ omnibus/config/software/sds.rb
@@ -2,18 +2,14 @@ name "sds"
 
 # default_version "4112a1c"
 default_version "main"
-
 source git: 'https://github.com/DataDog/dd-sensitive-data-scanner'
-
-relative_path "sds-go/rust"
 
 build do
     license "Apache-2.0"
     license_file "./LICENSE"
 
     if linux_target?
-        command "ls -lah"
-        command "cargo build --release"
-        command "cp target/release/libsds_go.so #{install_dir}/embedded/lib"
+        command "cargo build --release", cwd: "#{project_dir}/sds-go/rust"
+        command "cp sds-go/rust/target/release/libsds_go.so #{install_dir}/embedded/lib"
     end
 end
