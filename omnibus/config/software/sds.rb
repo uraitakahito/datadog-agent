82ceef5c7ed91e6cce3bdfffb690d4146e07384c
Remy Mathieu <remy.mathieu@datadoghq.com>
2024-04-04 10:02:10 +0200

    Also build the library on macOS

--- omnibus/config/software/sds.rb
+++ omnibus/config/software/sds.rb
@@ -8,8 +8,14 @@ build do
     license "Apache-2.0"
     license_file "./LICENSE"
 
-    if linux_target?
+    if linux_target? || osx_target?
         command "cargo build --release", cwd: "#{project_dir}/sds-go/rust"
-        command "cp sds-go/rust/target/release/libsds_go.so #{install_dir}/embedded/lib"
+
+        if osx_target?
+            command "cp sds-go/rust/target/release/libsds_go.dylib #{install_dir}/embedded/lib"
+        end
+        if linux_target?
+            command "cp sds-go/rust/target/release/libsds_go.so #{install_dir}/embedded/lib"
+        end
     end
 end
