715ffe913be70b429a3c09593aaf1a3e049824a4
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-04-25 14:24:49 +0200

    fix issues raised by mutatest

--- tasks/unit_tests.py
+++ tasks/unit_tests.py
@@ -8,4 +8,7 @@ def invoke_unit_tests(ctx):
     """
     Run the unit tests on the invoke tasks
     """
-    ctx.run(f"{sys.executable} -m unittest discover -s tasks -p '*_tests.py'", env={"GITLAB_TOKEN": "fake_token"})
+    ctx.run(
+        f"mutatest -t '{sys.executable} -m unittest discover -s tasks -p \"*_tests.py\"'",
+        env={"GITLAB_TOKEN": "fake_token"},
+    )
