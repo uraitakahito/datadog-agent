98b9f912a88131a72df56e06be64ae7250791ac8
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-09-04 17:16:30 +0200

    exit ok

--- tasks/winbuildscripts/test.ps1
+++ tasks/winbuildscripts/test.ps1
@@ -3,13 +3,13 @@ $ErrorActionPreference = "Stop"
 $Env:Python2_ROOT_DIR=$Env:TEST_EMBEDDED_PY2
 $Env:Python3_ROOT_DIR=$Env:TEST_EMBEDDED_PY3
 $tmpfile = [System.IO.Path]::GetTempFileName()
-& "C:\mnt\tools\ci\aws_ssm_get_wrapper.ps1" "$Env:DOCKER_REGISTRY_LOGIN_SSM_KEY" > $tmpfile
+& "C:\mnt\tools\ci\aws_ssm_get_wrapper.ps1" "$Env:DOCKER_REGISTRY_LOGIN_SSM_KEY" "$tmpfile"
 If ($LASTEXITCODE -ne "0") {
     exit $LASTEXITCODE
 }
 $DOCKER_REGISTRY_LOGIN = $(cat $tmpfile)
 
-& "C:\mnt\tools\ci\aws_ssm_get_wrapper.ps1" "$Env:AGENT_GITHUB_APP_ID_SSM_NAME" > $tmpfile
+& "C:\mnt\tools\ci\aws_ssm_get_wrapper.ps1" "$Env:AGENT_GITHUB_APP_ID_SSM_NAME" "$tmpfile"
 If ($LASTEXITCODE -ne "0") {
     exit $LASTEXITCODE
 }
